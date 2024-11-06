2ca93cde803f86310bfc12613b7574e74961b1b2
Devashish Dhamne <Devashish@villaplus.com>
2024-11-06 16:01:49 +0530

    Day 2 - Straing calculator

---
+++ TddTaster.Core/Day 2/StringCalculator.cs
@@ -0,0 +1,50 @@
+﻿namespace TddTaster.Core.Day_2
+{
+    public class StringCalculator
+    {
+        public static int Add(string numbers)
+        {
+            if (numbers == null)
+                throw new ArgumentException("null not allowed");
+
+            if (numbers == string.Empty)
+                return 0;
+
+            return PerformAdditionFor(numbers);
+        }
+
+        private static int PerformAdditionFor(string numbers)
+        {
+            var delimeters = GetDelimiters(numbers).ToArray();
+
+            var numbersArray = numbers.Split(delimeters, StringSplitOptions.RemoveEmptyEntries);
+
+            var intNumbers = new List<int>();
+
+            foreach (var number in numbersArray)
+            {
+                if (int.TryParse(number, out int intNumber))
+                    intNumbers.Add(intNumber);
+            }
+
+            if (intNumbers.Any(x => x < 0))
+                throw new ArgumentException("negatives not allowed");
+
+            return intNumbers.Sum(x => x);
+        }
+
+        private static List<string> GetDelimiters(string numbers)
+        {
+            var delimiters = new List<string> { ",", "\n" };
+
+            if (numbers.StartsWith("//"))
+            {
+                numbers = numbers.Substring(2);
+                var customDelimiter = numbers.First().ToString();
+                delimiters.Add(customDelimiter);
+            }
+
+            return delimiters;
+        }
+    }
+}
---
+++ TddTaster.UnitTests/Day 2/StringCalculatorShould.cs
@@ -0,0 +1,75 @@
+﻿using FluentAssertions;
+using TddTaster.Core.Day_2;
+
+namespace TddTaster.UnitTests.Day_2
+{
+    public class StringCalculatorShould
+    {
+        [Theory]
+        [InlineData(null)]
+        public void ThrowException_WhenNumbersIsNull(string numbers)
+        {
+            Assert.Throws<ArgumentException>(() => StringCalculator.Add(numbers));
+        }
+
+        [Fact]
+        public void ReturnZero_WhenNumbersAreEmpty()
+        {
+            var addition = StringCalculator.Add(string.Empty);
+
+            addition.Should().Be(0);
+        }
+
+        [Theory]
+        [InlineData("0", 0)]
+        [InlineData("1", 1)]
+        [InlineData("9", 9)]
+        public void ReturnNumber_WhenSingleNumberInNumbers(string numbers, int expectedAddition)
+        {
+            var addition = StringCalculator.Add(numbers);
+
+            addition.Should().Be(expectedAddition);
+        }
+
+        [Theory]
+        [InlineData("0,1", 1)]
+        [InlineData("1,2", 3)]
+        [InlineData("8,9", 17)]
+        [InlineData("0,5,9", 14)]
+        [InlineData("0,7,8,9", 24)]
+        [InlineData("0,1,2,3,4", 10)]
+        public void ReturnSumGivenMoreThanOneNumberSplitByCommas(string numbers, int expectedAddition)
+        {
+            var addition = StringCalculator.Add(numbers);
+
+            addition.Should().Be(expectedAddition);
+        }
+
+        [Theory]
+        [InlineData("-1")]
+        [InlineData("1,-2")]
+        [InlineData("-1,-2")]
+        [InlineData("1\n-2")]
+        [InlineData("-2\n1")]
+        [InlineData("//.\n-1.2.-3")]
+        public void ThrowException_WhenNegativeNumberInNumbers(string numbers)
+        {
+            Assert.Throws<ArgumentException>(() => StringCalculator.Add(numbers));
+        }
+
+        [Theory]
+        [InlineData("//.\n1.2", 3)]
+        [InlineData("///\n1/2", 3)]
+        [InlineData("//x\n1x2", 3)]
+        [InlineData("//;\n1;2", 3)]
+        [InlineData("//.\n1.2,3", 6)]
+        public void ReturnAddition_WhenMultipleNumbers_SplitByCustomDelimiter(string numbers, int expected)
+        {
+            var result = StringCalculator.Add(numbers);
+
+            result.Should().Be(expected);
+        }
+    }
+
+    
+}
